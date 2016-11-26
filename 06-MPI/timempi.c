2b5aab21f620fc88e4d7929dd6e174689d1f6041
Simon <3sweidma@informatik.uni-hamburg.de>
2016-11-26 14:22:53 +0100

    .c file finished first version.

--- 06-MPI/timempi.c
+++ 06-MPI/timempi.c
@@ -22,7 +22,8 @@ main(int argc, char **argv)
   int i; /* loop iterator */
   int amount_procs; /* total amount of processes */
 
-  char timestamp[30]; /* process individual timestamp */
+  const int timestamplength = 64;
+  char timestamp[timestamplength]; /* process individual timestamp */
 
   /* start processes*/
   ierr = MPI_Init(&argc, &argv);
@@ -37,24 +38,34 @@ main(int argc, char **argv)
   if (my_id == root_process) {
     for(i = 1; i < amount_procs; i++)
     {
-      ierr = MPI_Recv(timestamp, timestamp.length(), MPI_CHAR, MPI_ANY_SOURCE, return_data_tag, MPI_COMM_WORLD, &status);
+      ierr = MPI_Recv(timestamp, timestamplength, MPI_CHAR, MPI_ANY_SOURCE, return_data_tag, MPI_COMM_WORLD, &status);
       printf("%s\n", timestamp);
     }
   }
   else
   {
     char* hostname; /* hostname of current process */
-    char* curtime; /* current time */
 
-    struct timeval *tv; /* time */
+    struct timeval *tv; /* time struct */
+    time_t nowtime;
+    struct tm *nowtm;
+    char tmbuf[64];
+
 
     int err; /* debug error code */
 
     /* gets hostname and time */
     err = gethostname(hostname, 255);
     err = gettimeofday(tv,NULL);
+    nowtime = tv.tv_sec;
+    nowtm = localtime(&nowtime);
+    strftime(tmbuf, sizeof tmbuf, "%Y-%m-%d %H:%M:%S", nowtm);
+    snprintf(timestamp, sizeof timestamp, "%s: %s.%06d", hostname, tmbuf, tv.tv_usec);
 
-    
+    MPI_Send(timestamp, timestamplength, MPI_CHAR, root_process, return_data_tag, MPI_COMM_WORLD);
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  printf("Rang %d beendet jetzt!\n", my_id);
 
 }
