aea261ed1ffa3090e4bac931f58cb34e7b831a8e
Simon <3sweidma@informatik.uni-hamburg.de>
2016-11-26 16:30:53 +0100

    mpi slurm script

--- 06-MPI/timempi.c
+++ 06-MPI/timempi.c
@@ -1,8 +1,3 @@
-
-
-
-
-
 #include <stdio.h>
 #include <mpi.h>
 #include <unistd.h>
@@ -12,33 +7,32 @@
 #define send_data_tag 1001
 #define return_data_tag 1002
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
   MPI_Status status;
 
-  int ierr; /* mpi error code */
   int root_process; /* id of root process */
   int my_id; /* id of the current process */
   int i; /* loop iterator */
   int amount_procs; /* total amount of processes */
 
   const int timestamplength = 64;
-  char timestamp[timestamplength]; /* process individual timestamp */
+  char timestamp[64]; /* process individual timestamp */
 
   /* start processes*/
-  ierr = MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
   root_process = 0;
 
   /* get process id and amount of total processes */
-  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-  ierr = MPI_Comm_size(MPI_COMM_WORLD, &amount_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+  MPI_Comm_size(MPI_COMM_WORLD, &amount_procs);
 
   /* root process: monitoring and printing */
   if (my_id == root_process) {
     for(i = 1; i < amount_procs; i++)
     {
-      ierr = MPI_Recv(timestamp, timestamplength, MPI_CHAR, MPI_ANY_SOURCE, return_data_tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(timestamp, timestamplength, MPI_CHAR, MPI_ANY_SOURCE, return_data_tag, MPI_COMM_WORLD, &status);
       printf("%s\n", timestamp);
     }
   }
@@ -51,21 +45,18 @@ main(int argc, char **argv)
     struct tm *nowtm;
     char tmbuf[64];
 
-
-    int err; /* debug error code */
-
     /* gets hostname and time */
-    err = gethostname(hostname, 255);
-    err = gettimeofday(tv,NULL);
-    nowtime = tv.tv_sec;
+    gethostname(hostname, 40);
+    gettimeofday(tv,NULL);
+    nowtime = tv->tv_sec;
     nowtm = localtime(&nowtime);
     strftime(tmbuf, sizeof tmbuf, "%Y-%m-%d %H:%M:%S", nowtm);
-    snprintf(timestamp, sizeof timestamp, "%s: %s.%06d", hostname, tmbuf, tv.tv_usec);
+    snprintf(timestamp, sizeof timestamp, "%s: %s.%06ld", hostname, tmbuf, tv->tv_usec);
 
     MPI_Send(timestamp, timestamplength, MPI_CHAR, root_process, return_data_tag, MPI_COMM_WORLD);
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
   printf("Rang %d beendet jetzt!\n", my_id);
-
+  MPI_Finalize();
 }
