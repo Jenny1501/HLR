6f76f7acb9f880567f1ee7200182a409229453e6
Simon <3sweidma@informatik.uni-hamburg.de>
2016-12-01 22:57:07 +0100

    add slurm script.
    Now does the first printing with issues:
    1: Processes on same node sometimes have same array.
    2: array length not correctly distributing.

--- 07-circle/circle.c
+++ 07-circle/circle.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
+
+#define printtag 2000
+#define circletag 2001
 
 int*
-init (int N)
+init (int my_N)
 {
-  // Todo
-  int* buf = malloc(sizeof(int) * N);
+  int* buf = malloc(sizeof(int) * my_N);
 
   srand(time(NULL));
 
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < my_N; i++)
   {
     buf[i] = rand() % 25; //do not modify % 25
   }
-
   return buf;
 }
 
@@ -29,9 +31,13 @@ int
 main (int argc, char** argv)
 {
   char arg[256];
-  int N;
-  int rank;
-  int* buf;
+  int N, my_N;
+  int rank, amount_procs, root_process;
+  int endcondition;
+  int* buf, *new_buf;
+  int i;
+
+  MPI_Status status;
 
   if (argc < 2)
   {
@@ -41,28 +47,60 @@ main (int argc, char** argv)
 
   sscanf(argv[1], "%s", arg);
 
+  /* start processes*/
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &amount_procs);
+
   // Array length
   N = atoi(arg);
-  buf = init(N);
-
-  // Todo: myrank
-  rank = 0;
-
-  printf("\nBEFORE\n");
-
-  for (int i = 0; i < N; i++)
+  my_N = N / amount_procs;
+  int rest = N % amount_procs;
+  if(rank < rest)
   {
-    printf ("rank %d: %d\n", rank, buf[i]);
+    my_N++;
   }
+  buf = init(my_N);
+
+  /* get process rank and amount of total processes */
+  root_process = 0;
+
+
+  /* root process: fetching data and printing*/
+  if (rank == root_process) {
+    printf("\nBEFORE\n");
+    printf("rank %d:", rank);
+    for (int j = 0; j < my_N; j++)
+    {
+      printf (" %d",buf[j]);
+    }
+    printf("\n");
+    /* After this printing, the root process will already be in the first iteration of circle
+      It seems dirty because it is dirty.*/
+    new_buf = buf;
+    for(i = 1; i < amount_procs; i++)
+    {
+      MPI_Recv(buf, my_N, MPI_INT, i, printtag, MPI_COMM_WORLD, &status);
+      printf("rank %d:", i);
+      for (int j = 0; j < my_N; j++)
+      {
+        printf (" %d", buf[j]);
+      }
+      printf("\n");
+    }
+  }
+  /* non root process: sending data to root for print */
+  else
+  {
+    MPI_Send(buf, my_N, MPI_INT, root_process, printtag, MPI_COMM_WORLD);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
 
   circle(buf);
 
-  printf("\nAFTER\n");
+  //printf("\nAFTER\n");
 
-  for (int j = 0; j < N; j++)
-  {
-    printf ("rank %d: %d\n", rank, buf[j]);
-  }
-
+  MPI_Finalize();
   return EXIT_SUCCESS;
 }
